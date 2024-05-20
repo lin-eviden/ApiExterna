646d42ab332b101e33ab825796c3dd617c2c67ca
Lin~ <lin.lu.external@eviden.com>
2024-05-20 09:55:00 +0200

    añadir anotaciones para conexion front

--- src/main/java/com/example/demo/controller/ControladorProducto.java
+++ src/main/java/com/example/demo/controller/ControladorProducto.java
@@ -1,14 +1,17 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.business.GestorProducto;
-
 @RestController
+@RequestMapping("")
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT})
 public class ControladorProducto {
 
 	@Autowired
